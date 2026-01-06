5dbab9ddd81dd8100f1a394172081f80b4ac8a84
Marc Fehling <mafehling.git@gmail.com>
2026-01-06 19:52:07 +0100

    Let indent stage deal with it.

--- contrib/utilities/detect_merges_from_master_in_feature_branch.sh
+++ contrib/utilities/detect_merges_from_master_in_feature_branch.sh
@@ -15,29 +15,31 @@
 
 #
 # This is a little script that checks if the feature branch is linear,
-# i.e., no merges of the parent branch into the current branch are present.
+# i.e., no merges of the parent branch into the feature branch are present.
 #
-# The parent branch to check against can be supplied as a parameter to
-# this script, or otherwise it will be set to 'master' by default.
+# The feature branch as well as the parent branch to check against can
+# be supplied as parameters to this script, or otherwise they will be set
+# to 'HEAD' and 'master' by default, respectively.
 #
-# NOTE: This script will do nothing on the parent branch since the log
-#       will always be empty.
+# NOTE: This script will do nothing if feature and parent branch are the
+#       same since the log will always be empty.
 #
 
-branch="${1:-master}"
+feature_ref="${1:-HEAD}"
+parent_ref="${2:-master}"
 
 get_merge_commits_since_parent_branch () {
-  echo "$(git log --merges --pretty=format:"%h" $branch..)"
+  echo "$(git log --merges --pretty=format:"%h" ${parent_ref}..${feature_ref})"
 }
 
 get_commit_parents () {
   result=$(git rev-list --parents -n1 $1)
   # first result is the commit itself, which we omit
-  echo "$(cut --delimiter=' '  --fields=1 --complement <<< $result)"
+  echo "$(cut --delimiter=' '  --fields=1 --complement <<< ${result})"
 }
 
 commit_is_in_parent_branch () {
-  return $(git merge-base --is-ancestor $1 $branch)
+  return $(git merge-base --is-ancestor $1 ${parent_ref})
 }
 
 readarray -t merge_hash_array < <(get_merge_commits_since_parent_branch)
@@ -47,15 +49,15 @@ if [[ -z "${merge_hash_array}" ]] ; then
     exit 0
 fi
 
-echo "Merge commits found, checking if they originate from $branch."
+echo "Merge commits found, checking if they originate from ${parent_ref}."
 
 for hash in "${merge_hash_array[@]}"; do
-  readarray -d' ' merge_parents < <(get_commit_parents $hash)
+  readarray -d' ' merge_parents < <(get_commit_parents ${hash})
 
-  for parent in "${merge_parents[@]}"; do
-    if (commit_is_in_parent_branch $parent) ; then
-      echo "There is a merge commit coming from $branch!"
-      echo "The commit hash is $parent"
+  for parent_hash in "${merge_parents[@]}"; do
+    if (commit_is_in_parent_branch ${parent_hash}) ; then
+      echo "There is a merge commit coming from ${parent_ref}!"
+      echo "The commit hash is ${hash}"
       exit 1
     fi
   done
